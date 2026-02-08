693caa4c9f198b26ed9a263f1f2df38c64a03f9e
Ken Española <revas4192@gmail.com>
2026-02-08 19:30:24 +0800

    initial commit

--- cloudflare/_worker.js
+++
@@ -1,7 +0,0 @@
-export default {
-  async scheduled(event, env, ctx) {
-    const url = "https://concord-03cc.onrender.com/"; // Replace with your Render URL
-    await fetch(url);
-    console.log(`Pinged: ${url}`);
-  },
-};
