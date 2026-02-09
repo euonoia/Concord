91179474324e999a40d18f63225cd6928f8967ae
Ken Española <revas4192@gmail.com>
2026-02-09 17:10:15 +0800

    Update database.php

--- config/database.php
+++ config/database.php
@@ -58,17 +58,12 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-          'options' => extension_loaded('pdo_mysql') ? array_filter([
-                
+           'options' => extension_loaded('pdo_mysql') ? array_filter([
+               
                 (defined('Pdo\Mysql::ATTR_SSL_CA') ? \Pdo\Mysql::ATTR_SSL_CA : \PDO::MYSQL_ATTR_SSL_CA) 
-                    => env('MYSQL_ATTR_SSL_CA') 
-                        ? (str_starts_with(env('MYSQL_ATTR_SSL_CA'), '/') 
-                            ? env('MYSQL_ATTR_SSL_CA')  
-                            : base_path(env('MYSQL_ATTR_SSL_CA'))) 
-                        : null,
-            
-                (defined('Pdo\Mysql::ATTR_SSL_VERIFY_SERVER_CERT') ? \Pdo\Mysql::ATTR_SSL_VERIFY_SERVER_CERT : \PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT) 
-                    => false,
+                    => env('MYSQL_ATTR_SSL_CA') ? base_path(env('MYSQL_ATTR_SSL_CA')) : null,
+                
+                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
             ]) : [],
         ],
 
