12eca8e64172cf870f972ed764ab61806f2c3f5a
Ken Española <revas4192@gmail.com>
2026-02-09 18:07:08 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,15 @@ Route::get('/', function () {
 });
 
 Route::resource('tasks', TaskController::class);
+
+Route::get('/_ssl-check', function () {
+    return [
+        'ca_exists' => file_exists('/etc/secrets/isrgrootx1.pem'),
+        'ca_readable' => is_readable('/etc/secrets/isrgrootx1.pem'),
+        'openssl' => extension_loaded('openssl'),
+        'pdo_mysql' => extension_loaded('pdo_mysql'),
+        'ca_hash' => file_exists('/etc/secrets/isrgrootx1.pem')
+            ? hash_file('sha256', '/etc/secrets/isrgrootx1.pem')
+            : null,
+    ];
+});
