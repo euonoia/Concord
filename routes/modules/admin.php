9a48f773c07e362703b68acd59694ad6a028a127
Ken Española <revas4192@gmail.com>
2026-02-26 23:00:43 +0800

    Hr2 (#37)
    * initial commit
    
    * initial commit
    
    * Update succession.blade.php
    
    * initial commit

--- routes/modules/admin.php
+++ routes/modules/admin.php
@@ -1,5 +1,6 @@
 <?php
 // routes/modules/admin.php
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\admin\Hr\hr2\AdminLearningController;
 use App\Http\Controllers\admin\Hr\hr2\CompetencyController;
@@ -7,27 +8,42 @@ use App\Http\Controllers\admin\Hr\hr2\AdminTrainingController;
 use App\Http\Controllers\admin\Hr\hr2\AdminSuccessionController;
 use App\Http\Controllers\admin\Hr\hr2\AdminEssController;
 
+// --- Admin Dashboard ---
 Route::get('/dashboard', function () {
     return view('admin.dashboard');
 })->name('admin.dashboard');
 
-Route::get('/hr2/departments/{dept_code}/specializations', [AdminSuccessionController::class, 'getSpecializations'])
-    ->name('departments.specializations');
+// --- HR2 Department AJAX Endpoints ---
+Route::prefix('hr2')->group(function () {
+    // Specializations by Department
+    Route::get('/departments/{dept_code}/specializations', [AdminSuccessionController::class, 'getSpecializations'])
+        ->name('departments.specializations');
 
-// --- Hr2 Admin ---
+    // Positions by Department + Specialization
+    Route::get('/departments/{dept_code}/positions', [AdminSuccessionController::class, 'getPositions'])
+        ->name('departments.positions');
+});
+
+// --- HR2 Admin Resource Routes ---
 Route::resource('competencies', CompetencyController::class);
 Route::resource('learning', AdminLearningController::class);
 Route::resource('training', AdminTrainingController::class);
+
+// --- Succession Planning Routes ---
 Route::prefix('succession')->group(function () {
     Route::get('/', [AdminSuccessionController::class, 'index'])->name('succession.index');
+
+    // Positions
     Route::post('/position', [AdminSuccessionController::class, 'storePosition'])->name('succession.position.store');
-    Route::post('/candidate', [AdminSuccessionController::class, 'storeCandidate'])->name('succession.candidate.store');
     Route::delete('/position/{id}', [AdminSuccessionController::class, 'destroyPosition'])->name('succession.position.destroy');
+
+    // Candidates
+    Route::post('/candidate', [AdminSuccessionController::class, 'storeCandidate'])->name('succession.candidate.store');
     Route::delete('/candidate/{id}', [AdminSuccessionController::class, 'destroyCandidate'])->name('succession.candidate.destroy');
 });
+
+// --- ESS Module Routes ---
 Route::prefix('ess')->group(function () {
     Route::get('/', [AdminEssController::class, 'index'])->name('ess.index');
     Route::patch('/{id}/status', [AdminEssController::class, 'updateStatus'])->name('ess.updateStatus');
 });
-Route::get('/hr2/departments/{dept_code}/specializations', [AdminSuccessionController::class, 'getSpecializations'])
-    ->name('departments.specializations');
