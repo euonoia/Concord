63060da65c676ceccb6e3d06182c25f71048035f
Ken Española <revas4192@gmail.com>
2026-03-01 23:07:01 +0800

    Update AdminAttendanceController.php

--- app/Http/Controllers/admin/Hr/hr3/AdminAttendanceController.php
+++ app/Http/Controllers/admin/Hr/hr3/AdminAttendanceController.php
@@ -15,7 +15,7 @@ class AdminAttendanceController extends Controller
             Cache::put("attendance_token_$token", true, now()->addSeconds(30));
 
             // Update the QR to point to the new auto-verify route
-            $qrValue = route('attendance.auto_verify', ['token' => $token]);
+            $qrValue = route('attendance.verify', ['token' => $token]);
 
             return view('admin.hr3.attendance_station', compact('token', 'qrValue'));
         }
