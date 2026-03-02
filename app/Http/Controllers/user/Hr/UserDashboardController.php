005cce1d213f8aef73f9bfe1b1afa5c6dcb09525
Ken Española <revas4192@gmail.com>
2026-03-02 17:30:19 +0800

    initial commit

--- app/Http/Controllers/user/Hr/UserDashboardController.php
+++ app/Http/Controllers/user/Hr/UserDashboardController.php
@@ -4,22 +4,24 @@ namespace App\Http\Controllers\user\Hr;
 
 use App\Http\Controllers\Controller;
 use App\Models\Employee;
+use App\Models\admin\Hr\hr3\Shift;
 use Illuminate\Support\Facades\Auth;
+use Carbon\Carbon;
 
 class UserDashboardController extends Controller
 {
     public function index()
-    {
-     
-        $employee = Employee::where('user_id', Auth::id())->first();
+        {
+            $employee = Employee::with('position')->where('user_id', Auth::id())->first();
+            $allShifts = collect(); 
 
-  
-        if (!$employee) {
-           
-            $employee = new Employee();
+            if ($employee) {
+                $allShifts = \App\Models\admin\Hr\hr3\Shift::where('employee_id', $employee->employee_id)
+                    ->where('is_active', 1)
+                    ->orderByRaw("FIELD(day_of_week, 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday')")
+                    ->get();
+            }
+
+            return view('hr.dashboard', compact('employee', 'allShifts'));
         }
-
-       
-        return view('hr.dashboard', compact('employee'));
-    }
 }
