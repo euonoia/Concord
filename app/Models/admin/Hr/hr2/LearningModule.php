f671bb244d02f7e7891a980065241addcacccd3e
Ken Española <revas4192@gmail.com>
2026-03-08 23:41:31 +0800

    initial commit

--- app/Models/admin/Hr/hr2/LearningModule.php
+++ app/Models/admin/Hr/hr2/LearningModule.php
@@ -1,10 +1,8 @@
 <?php
-
 namespace App\Models\admin\Hr\hr2;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use App\Models\admin\Hr\hr2\CourseEnroll;
 
 class LearningModule extends Model
 {
@@ -15,16 +13,18 @@ class LearningModule extends Model
     public $timestamps = true;
 
     protected $fillable = [
-        'title',
-        'description',
-        'competency_id',
-        'learning_type',
-        'duration',
+        'module_code',
+        'module_name',
+        'dept_code',
+        'specialization_name',
+        'module_type',
+        'duration_hours',
+        'is_mandatory',
     ];
 
-    // Relationship to enrolls
+    // Relationship to employee enrollments
     public function enrolls()
     {
-        return $this->hasMany(CourseEnroll::class, 'course_id', 'id');
+        return $this->hasMany(CourseEnroll::class, 'module_id', 'id');
     }
 }
