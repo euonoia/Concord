2abc9bf3e7ea99e4da029f7ee18bd7ab16e2fc09
Aristaeus <156321439+Darktodoom123@users.noreply.github.com>
2026-03-09 13:45:27 +0800

    fix missing icons

--- resources/views/core/core1/layouts/app.blade.php
+++ resources/views/core/core1/layouts/app.blade.php
@@ -5,9 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'Hospital Management System')</title>
-
     <link rel="stylesheet" href="{{ asset('css/core1/example.css') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     @stack('styles')
 </head>
 <body>
