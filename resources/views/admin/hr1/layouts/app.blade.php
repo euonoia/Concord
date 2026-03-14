a6d413b640ad58f9e896087600563d053ac7e0b4
lew <jaysonpinggoy11@gmail.com>
2026-03-14 10:38:03 +0800

    Update app.blade.php

--- resources/views/admin/hr1/layouts/app.blade.php
+++ resources/views/admin/hr1/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>HR2 Module</title>
+    <title>HR1 Module</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
