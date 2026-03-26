7c696fd939aa70b1d735d25b87e9627dc9cd2a65
Ken Española <revas4192@gmail.com>
2026-03-26 21:10:26 +0800

    Cleaning (#129)
    * initial commit
    
    cleaned the root
    
    * inital commit
    
    * Stop tracking database migrations
    
    * Update .gitignore
    
    * initial commit
    
    * Update hr.php

--- routes/modules/hr.php
+++ routes/modules/hr.php
@@ -1,4 +1,5 @@
 <?php
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\user\Hr\UserDashboardController;
 use App\Http\Controllers\user\Hr\hr2\UserCompetencyController;
@@ -8,7 +9,8 @@ use App\Http\Controllers\user\Hr\hr2\UserSuccessionController;
 use App\Http\Controllers\user\Hr\hr2\UserEssController;
 use App\Http\Controllers\user\Hr\hr2\UserLearningMaterialsController;
 use App\Http\Controllers\user\Hr\hr2\UserPayrollController;
-// Import the new HR3 Controller
+
+// HR3 Controllers
 use App\Http\Controllers\user\Hr\hr3\UserAttendanceController;
 use App\Http\Controllers\user\Hr\hr3\UserClaimsController;
 
@@ -36,7 +38,7 @@ Route::prefix('learning')->middleware(['auth'])->group(function () {
     Route::post('/enroll/{module_code}', [UserLearningController::class, 'enroll'])
         ->name('user.learning.enroll');
 
-    // 2. Learning Materials (for enrolled courses)
+    // Learning Materials
     Route::get('/materials', [UserLearningMaterialsController::class, 'index'])
         ->name('user.learning.materials.index');
 
@@ -44,26 +46,25 @@ Route::prefix('learning')->middleware(['auth'])->group(function () {
         ->name('user.learning.materials.show');
 
 });
-Route::post('/user/request-shift/{id}', [UserEssController::class, 'requestShift'])
-    ->name('user.shift.request');
-     Route::get('/payroll', [UserPayrollController::class, 'index'])->name('user.payroll.index');
-    Route::post('/payroll/store', [UserPayrollController::class, 'store'])->name('user.payroll.store');
 
-// ESS Payroll Requests (Employee)
-Route::get('/ess-payroll', [UserEssController::class, 'payrollIndex'])->name('user.ess.payroll.index');
-Route::post('/ess-payroll/store', [UserEssController::class, 'payrollStore'])->name('user.ess.payroll.store');
-// --- END OF HR2 Department ---
 
-// --- START OF HR3 Department (Attendance & Timekeeping) ---
+/*
+|--------------------------------------------------------------------------
+| HR3 - ATTENDANCE & TIMEKEEPING
+|--------------------------------------------------------------------------
+*/
+
+// QR / Face / Attendance
 Route::get('/attendance/scan', [UserAttendanceController::class, 'scanView'])
     ->name('user.attendance.scan');
+
 Route::match(['get', 'post'], '/attendance/verify', [UserAttendanceController::class, 'verify'])
     ->name('attendance.verify');
+
 Route::get('/attendance/success', [UserAttendanceController::class, 'success'])
     ->name('user.attendance.success');
- // ESS Requests / Leave / Profile / Document Requests
-    Route::get('/ess', [UserEssController::class, 'index'])->name('user.ess.index');
-    Route::post('/ess/store', [UserEssController::class, 'store'])->name('user.ess.store');
-    // Claims / Reimbursement
-    Route::post('/claims/store', [UserClaimsController::class, 'store'])->name('user.claims.store');
-// --- END OF HR3 Department ---
+
+
+// Claims / Reimbursements
+Route::post('/claims/store', [UserClaimsController::class, 'store'])
+    ->name('user.claims.store');
